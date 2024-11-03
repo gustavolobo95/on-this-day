b62f38218592556805a9dd63de7031980eb98c63
Gustavo Lobo <gustavo.lobo95@gmail.com>
2024-11-03 20:51:29 -0300

    Criando service para validação de datas

---
+++ src/main/java/com/lobo/onThisDay/service/DateValidatorService.java
@@ -0,0 +1,26 @@
+package com.lobo.onThisDay.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeParseException;
+
+/**
+ *
+ * Classe que fará validação de datas suportadas pela API
+ *
+ * @author Gustavo Lobo
+ */
+@Service
+public class DateValidatorService {
+
+    public boolean supportsDateFormat(String date) {
+        try {
+            YearMonth dateFormatted = YearMonth.parse(date);
+            return true;
+        } catch (DateTimeParseException e) {
+            throw new RuntimeException(String.format("Data informada não tem formato suportado: %s", date));
+        }
+    }
+
+}
