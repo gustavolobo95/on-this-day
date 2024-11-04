2a97b5fbb06926011a5b68bd39de11de878924cc
Gustavo Lobo <gustavo.lobo95@gmail.com>
2024-11-03 21:54:42 -0300

    Ajustando para objeto de mês-dia para validar data

--- src/main/java/com/lobo/onThisDay/service/DateValidatorService.java
+++ src/main/java/com/lobo/onThisDay/service/DateValidatorService.java
@@ -2,7 +2,7 @@ package com.lobo.onThisDay.service;
 
 import org.springframework.stereotype.Service;
 
-import java.time.YearMonth;
+import java.time.MonthDay;
 import java.time.format.DateTimeParseException;
 
 /**
@@ -16,7 +16,10 @@ public class DateValidatorService {
 
     public boolean supportsDateFormat(String date) {
         try {
-            YearMonth dateFormatted = YearMonth.parse(date);
+            // TODO: Verificar um objeto que possa representar data no formato de "dd/MM", essa apesar de cumprir o
+            //  proposito precisa que a data venha no formato "--MM-dd".
+
+            MonthDay dateFormatted = MonthDay.parse(date);
             return true;
         } catch (DateTimeParseException e) {
             throw new DateTimeParseException("Data informada não tem formato suportado: ", date, 0);
