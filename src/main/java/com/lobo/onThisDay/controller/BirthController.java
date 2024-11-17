2412354b49c22aad884312455b7269d103c682fc
Gustavo Lobo <gustavo.lobo95@gmail.com>
2024-11-17 20:27:33 -0300

    Alterando retorno do request.

--- src/main/java/com/lobo/onThisDay/controller/BirthController.java
+++ src/main/java/com/lobo/onThisDay/controller/BirthController.java
@@ -1,18 +1,16 @@
 package com.lobo.onThisDay.controller;
 
-import com.lobo.onThisDay.model.Person;
 import com.lobo.onThisDay.service.BirthService;
 import com.lobo.onThisDay.service.DateValidatorService;
+import com.lobo.onThisDay.service.MonthDayConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.MonthDay;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -26,6 +24,9 @@ public class BirthController {
 
     @Autowired
     DateValidatorService dateValidatorService;
+
+    @Autowired
+    MonthDayConverter monthDayConverter;
 
     @Autowired
     BirthService birthService;
@@ -42,14 +43,13 @@ public class BirthController {
      */
     @GetMapping("/getPersonsBornIn")
     public ResponseEntity<List<?>> getPersonsBornIn(@RequestParam String birthDate) {
-        // TODO: aplicar validação em data e criar testes.
         try {
             if (dateValidatorService.supportsDateFormat(birthDate)) {
-                MonthDay date = MonthDay.parse(birthDate);
-                birthService.getPersonsBornIn(date);
+                MonthDay date = monthDayConverter.convert(birthDate);
+                return ResponseEntity.ok(birthService.getPersonsBornIn(date));
             }
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().body(Collections.singletonList(e.getMessage()));
         }
         return null;
     }
