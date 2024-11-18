2fa247161a075847209ec857650d9efd92773eb1
Gustavo Lobo <gustavo.lobo95@gmail.com>
2024-11-18 17:24:06 -0300

    REFACTOR: Adicionando logger e fazendo tratamento de erro.

--- src/main/java/com/lobo/onThisDay/service/OnThisDayScrapperService.java
+++ src/main/java/com/lobo/onThisDay/service/OnThisDayScrapperService.java
@@ -5,9 +5,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.time.Month;
 import java.time.MonthDay;
 import java.util.ArrayList;
@@ -25,6 +26,7 @@ public class OnThisDayScrapperService {
     // TODO: Talvez alterar a url base para ser especifica para nascimentos, outros eventos podem ter uma url em formato diferente.
     private static final String BASE_URL = "https://www.onthisday.com/%s/%s/%d";
     private static final String BIRTH_EVENTS_URL = "birthdays";
+    private static final Logger LOGGER = LoggerFactory.getLogger(OnThisDayScrapperService.class);
 
     /**
      * Esse metodo será o responsável por fazer os requests da lista de nascidos na fonte do onthisday, de inicio estamos
@@ -35,7 +37,7 @@ public class OnThisDayScrapperService {
      * @param date
      * @return lista de pessoas nascidas em "x" dia.
      */
-    public List<Person> getHighlightPersonsBornIn(MonthDay date) throws IOException {
+    public List<Person> getHighlightPersonsBornIn(MonthDay date) {
         int day = date.getDayOfMonth();
         Month month = date.getMonth();
 
@@ -43,18 +45,19 @@ public class OnThisDayScrapperService {
         String livingPeriodOrAge;
         List<Person> returnedPersons = new ArrayList<>();
 
-        Document document = Jsoup.connect(String.format(BASE_URL, BIRTH_EVENTS_URL, month.toString(), day)).get();
+        try {
+            Document document = Jsoup.connect(String.format(BASE_URL, BIRTH_EVENTS_URL, month.toString(), day)).get();
 
-        // Pegando todas as pessoas que são marcadas como destaque:
-        Elements highlights = document.getElementsByClass("section section--highlight section--poi section--poi-b");
+            // Pegando todas as pessoas que são marcadas como destaque:
+            Elements highlights = document.getElementsByClass("section section--highlight section--poi section--poi-b");
 
-        for(Element personElement : highlights) {
+            for(Element personElement : highlights) {
 
-            Person person = new Person();
+                Person person = new Person();
 
-            Elements spanTitles = personElement.getElementsByClass("poi__heading-txt");
+                Elements spanTitles = personElement.getElementsByClass("poi__heading-txt");
 
-            for(Element title : spanTitles) {
+                for(Element title : spanTitles) {
                     String[] nameAndDate = title.toString().split("<span class=\"poi__date\">");
                     if (nameAndDate.length == 2) {
                         personName = nameAndDate[0].replaceAll("<span class=\"poi__heading-txt\">", "").trim();
@@ -62,21 +65,26 @@ public class OnThisDayScrapperService {
                         person.setName(personName);
                         person.setLivingPeriodOrAge(livingPeriodOrAge);
                     }
+                }
+
+                Elements gridToGetDescription = personElement.getElementsByClass("grid__item one-half--768 five-twelfths--1024");
+
+                Element grid = gridToGetDescription.stream()
+                        .findFirst()
+                        .orElseThrow(() -> new RuntimeException("Não foi encontrado nenhum destaque."));
+
+                String descriptionUnformmated = grid.getElementsByTag("p").toString();
+
+                String description = formatDescriptionElement(descriptionUnformmated);
+
+                person.setDescription(description);
+
+                returnedPersons.add(person);
             }
 
-            Elements gridToGetDescription = personElement.getElementsByClass("grid__item one-half--768 five-twelfths--1024");
-
-            Element grid = gridToGetDescription.stream()
-                                                .findFirst()
-                                                .orElseThrow(() -> new RuntimeException("Não foi encontrado nenhum destaque."));
-
-            String descriptionUnformmated = grid.getElementsByTag("p").toString();
-
-            String description = formatDescriptionElement(descriptionUnformmated);
-
-            person.setDescription(description);
-
-            returnedPersons.add(person);
+        } catch (Exception e) {
+            LOGGER.error("Ocorreu um erro durante o web scrapping: {}", e.getMessage());
+            throw new RuntimeException(e.getMessage());
         }
 
         return returnedPersons;
