53be99fce7eadd00495b37c54545cecfee17acb5
Gustavo Lobo <gustavo.lobo95@gmail.com>
2025-01-26 09:18:16 -0300

    REFACTOR: Refatorando metodos

--- src/main/java/com/lobo/onThisDay/service/OnThisDayScrapperService.java
+++ src/main/java/com/lobo/onThisDay/service/OnThisDayScrapperService.java
@@ -16,7 +16,6 @@ import java.time.MonthDay;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 
 /**
